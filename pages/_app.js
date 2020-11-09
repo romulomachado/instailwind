18946319f45e59874a7caf127c955661d99fecea
Rômulo Machado <romulo.machado5@gmail.com>
2020-11-09 15:21:09 -0300

    Layout with header and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import 'tailwindcss/tailwind.css';
 
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='flex flex-col h-screen'>
+      <Header />
+
+      <main className='flex-1'>
+        <Component {...pageProps} />
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-yellow-700">Instagram</h1>
-  )
+    <p>Home</p>
+  );
 }
