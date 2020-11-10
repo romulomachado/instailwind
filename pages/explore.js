7695e21c04479409bb077c59ae7dc2be78ef4500
Rômulo Machado <romulo.machado5@gmail.com>
2020-11-10 09:11:42 -0300

    Search header

--- pages/explore.js
+++ pages/explore.js
@@ -3,7 +3,17 @@ import Header from '../components/Header';
 export default function Explore() {
   return (
     <div>
-      <Header></Header>
+      <Header>
+        <input
+          autocapitalize='none'
+          autocomplete='off'
+          placeholder='Search'
+          spellcheck='true'
+          type='search'
+          value=''
+          className='w-full py-3 text-sm text-center placeholder-gray-500 border rounded-md outline-none'
+        />
+      </Header>
       <p>Explore</p>
     </div>
   );
