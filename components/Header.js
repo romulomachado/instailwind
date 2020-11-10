5a8474e5123a574b41c5cb9b94ba3531565cabd0
Rômulo Machado <romulo.machado5@gmail.com>
2020-11-10 10:29:02 -0300

    Adjust padding on header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export default function Footer({ children }) {
   return (
-    <header className='flex justify-center h-12 p-3 border-b'>
+    <header className='flex justify-center h-12 px-4 py-2 border-b'>
       {children}
     </header>
   )
