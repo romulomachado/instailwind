59b4671a1b3c5e23d9a5a0fa25622f941bd958b9
Rômulo Machado <romulo.machado5@gmail.com>
2020-11-10 10:18:19 -0300

    Scroll feed and don't lose Nav

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Nav from '../components/Nav';
 function MyApp({ Component, pageProps }) {
   return (
     <div className='flex flex-col h-screen'>
-      <main className='flex-1'>
+      <main className='flex-1 overflow-scroll'>
         <Component {...pageProps} />
       </main>
 
