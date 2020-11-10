ec3da6b90d14f41327c4b8d367c227e580c5dece
Rômulo Machado <romulo.machado5@gmail.com>
2020-11-10 08:46:48 -0300

    Header is not shared

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,15 @@
 import 'tailwindcss/tailwind.css';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Nav from '../components/Nav';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className='flex flex-col h-screen'>
-      <Header />
-
       <main className='flex-1'>
         <Component {...pageProps} />
       </main>
 
-      <Footer />
+      <Nav />
     </div>
   );
 }
